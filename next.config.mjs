1df353c04e8980c36578efc3365fa4d76e8a6840
Leonid Deev <ltupov2003@yandex.ru>
2024-06-16 22:57:33 +0300

    update env host 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        HTTP_VIDEO_DOWNLOAD: 'http://caddy:5291/api/videos/download', // адрес пополнения БД без модерации
-        WS_VIDEO_DOWNLOAD: 'http://caddy:5291/api/status/currentStatus', // адрес проверки статуса пополнения
-        HTTP_VIDEO_MODERATION: 'http://caddy:5291/api/videos/verify', // адрес пополнения БД с модерацией
-        WS_VIDEO_MODERATION: 'http://caddy:5291/api/status/currentStatus', // адрес проверки статуса модерации
+        HTTP_VIDEO_DOWNLOAD: 'https://3ce0-109-252-24-49.ngrok-free.app/api/videos/download', // адрес пополнения БД без модерации http://caddy:5291/api/videos/download
+        WS_VIDEO_DOWNLOAD: 'https://3ce0-109-252-24-49.ngrok-free.app/api/status/currentStatus', // адрес проверки статуса пополнения http://caddy:5291/api/status/currentStatus
+        HTTP_VIDEO_MODERATION: 'https://3ce0-109-252-24-49.ngrok-free.app/api/videos/verify', // адрес пополнения БД с модерацией http://caddy:5291/api/videos/verify
+        WS_VIDEO_MODERATION: 'https://3ce0-109-252-24-49.ngrok-free.app/api/status/currentStatus', // адрес проверки статуса модерации http://caddy:5291/api/status/currentStatus
     }
 };
 
