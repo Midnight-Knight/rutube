16b96d553d7c8007ff9270c264929570effc3fda
Leonid Deev <ltupov2003@yandex.ru>
2024-06-22 17:45:19 +0300

    fix env Vercel 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /*env: {
-        HTTP_VIDEO_DOWNLOAD: 'https://3ce0-109-252-24-49.ngrok-free.app/api/videos/download', // адрес пополнения БД без модерации http://caddy:5291/api/videos/download
-        WS_VIDEO_DOWNLOAD: 'https://3ce0-109-252-24-49.ngrok-free.app/api/status/currentStatus', // адрес проверки статуса пополнения http://caddy:5291/api/status/currentStatus
-        HTTP_VIDEO_MODERATION: 'https://3ce0-109-252-24-49.ngrok-free.app/api/videos/verify', // адрес пополнения БД с модерацией http://caddy:5291/api/videos/verify
-        WS_VIDEO_MODERATION: 'https://3ce0-109-252-24-49.ngrok-free.app/api/status/currentStatus', // адрес проверки статуса модерации http://caddy:5291/api/status/currentStatus
-    }*/
+    env: {
+        HTTP_VIDEO_DOWNLOAD: process.env.HTTP_VIDEO_DOWNLOAD_NEXT || 'http://caddy:5291/api/videos/download', // адрес пополнения БД без модерации http://caddy:5291/api/videos/download https://3ce0-109-252-24-49.ngrok-free.app/api/videos/download
+        WS_VIDEO_DOWNLOAD: process.env.WS_VIDEO_DOWNLOAD_NEXT || 'http://caddy:5291/api/status/currentStatus', // адрес проверки статуса пополнения http://caddy:5291/api/status/currentStatus https://3ce0-109-252-24-49.ngrok-free.app/api/status/currentStatus
+        HTTP_VIDEO_MODERATION: process.env.HTTP_VIDEO_MODERATION_NEXT || 'http://caddy:5291/api/videos/verify', // адрес пополнения БД с модерацией http://caddy:5291/api/videos/verify https://3ce0-109-252-24-49.ngrok-free.app/api/videos/verify
+        WS_VIDEO_MODERATION: process.env.WS_VIDEO_MODERATION_NEXT || 'http://caddy:5291/api/status/currentStatus', // адрес проверки статуса модерации http://caddy:5291/api/status/currentStatus https://3ce0-109-252-24-49.ngrok-free.app/api/status/currentStatus
+    }
 };
 
 export default nextConfig;
